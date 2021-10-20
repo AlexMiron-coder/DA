5956a1d29f7fcd5796abdf84a59d467999d36654
AlexMiron-coder <57828189+AlexMiron-coder@users.noreply.github.com>
2021-10-20 13:34:59 +0300

    Add files via upload

---
+++ lab1/cmake-build-debug/CMakeFiles/lab1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1.dir/benchmark.cpp.o"
+  "lab1"
+  "lab1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
